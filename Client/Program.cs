31947e00788d77ce8d31fd7722e1a22963cc37ec
Mr. Touraj Ostovari <46593765+TourajOstovari@users.noreply.github.com>
2021-05-23 00:43:55 +0430

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.Sockets.TcpClient tcpListener = new System.Net.Sockets.TcpClient(("127.0.0.1"), 8080);
+            tcpListener.NoDelay = true;
+            tcpListener.SendBufferSize = 2500;
+            System.Net.Sockets.Socket socket = tcpListener.Client;
+            System.Net.Sockets.NetworkStream nt = new System.Net.Sockets.NetworkStream(socket);
+            System.IO.StreamWriter wr = new System.IO.StreamWriter(nt);
+            string msg = " ";
+            while (msg != "")
+            {
+                Console.WriteLine($"Type your message:");
+                wr.WriteLine(Console.ReadLine());
+                wr.Flush();
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.Sockets.TcpListener  tcpListener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Parse("127.0.0.1"), 8080);
+            tcpListener.Server.NoDelay = true;
+            tcpListener.Server.ReceiveBufferSize = 2500;
+            tcpListener.Start();
+            System.Net.Sockets.Socket socket = tcpListener.AcceptSocket();
+            System.Net.Sockets.NetworkStream nt = new System.Net.Sockets.NetworkStream(socket);
+            System.IO.StreamReader wr = new System.IO.StreamReader(nt);
+            while (true)
+            {
+                Console.WriteLine($"your message: {wr.ReadLine()}");
+            }
+        }
+    }
+}
